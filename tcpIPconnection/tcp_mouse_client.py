56b39ecc472478e02fb6c7fa39c540e65fe79b8e
yias <iasonbatz@gmail.com>
2019-12-09 17:57:15 +0100

    stable tcp communication in tcpIPconnection

--- tcpIPconnection/tcp_mouse_client.py
+++ tcpIPconnection/tcp_mouse_client.py
@@ -19,11 +19,17 @@ def main():
 
 
 	# Connect the socket to the port where the server is listening
-	server_address = ('localhost', 10353)
+	server_address = ('localhost', 10351)
 
 	print('connecting to %s port %s', server_address)
 
 	sock.connect(server_address)
+
+	# define a header size of the message
+	HEADERSIZE=8
+
+	# define message identifier
+	msg_idf="!&?5"
 
 	msg=[]
 
@@ -33,16 +39,22 @@ def main():
 
 	startTime=time.time()
 
+	counter=0
+
 	while(True):
 		try: 
 			event=spnav.spnav_poll_event()
 			if event is not None:
 				msg=[event.translation[0],event.translation[1],event.translation[2]]
 				data=json.dumps({"a": 'translation', "b": msg})
-				sock.sendall(data.encode())
+				msg_len=('{:<'+str(HEADERSIZE)+'}').format(str(sys.getsizeof(data)))
+				msg_id='{:<8}'.format(str(counter))
+				sock.sendall(msg_idf.encode('utf-8')+msg_len.encode('utf-8')+msg_id.encode()+data.encode())
+
 				time_passed=time.time()-startTime
-				print('time ', time_passed, ', translation: ', msg, type(msg))
+				print('time ', time_passed, ', translation: ', msg, 'msgID', counter)
 				startTime=time.time()
+				counter+=1
 		except KeyboardInterrupt:
 			print('closing socket')
 			sock.close()
--- tcpIPconnection/tcp_server.py
+++ tcpIPconnection/tcp_server.py
@@ -24,7 +24,7 @@ def main():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	# Bind the socket to the port
-	server_address = ('localhost', 10353)
+	server_address = ('localhost', 10351)
 
 	print('starting up on %s port %s' % server_address)
 
@@ -33,9 +33,12 @@ def main():
 	# initialize client and adresses lists
 	connections_list=list() #np.empty([1,])
 	clients_address=list() #np.empty([1,])
+
+	# define buffer size
+	BUFFER_SIZE=4
 	
 	# define a header size of the message
-	HEADERSIZE=10
+	HEADERSIZE=8
 
 	# define message identifier
 	msg_idf="!&?5"
@@ -56,10 +59,26 @@ def main():
 			connection_exist=True;
 			print('connection from ', client_address)
 			while(True):
-				data=connection.recv(16)
-				print('received: %s' %data)
+				data=connection.recv(BUFFER_SIZE)
+				# print(type(data))
+				if data.decode('utf-8')==msg_idf:
+					print('new msg rcvd')
+					data=connection.recv(HEADERSIZE)
+					msg_len=int(data.decode('utf-8'))
+					print('expecting nb bytes: ', msg_len)
+					data=connection.recv(HEADERSIZE)
+					msg_id=int(data.decode('utf-8'))
+					print('msg id: ',msg_id)
+					
+					full_msg='';
+					while sys.getsizeof(full_msg)<msg_len:
+						data=connection.recv(BUFFER_SIZE)
+						full_msg=full_msg+data
+					msg_data=json.loads(full_msg.decode('utf-8'))
+					print('%s: %s' %(msg_data.get("a"),msg_data.get("b")))
+
 				if(sock.fileno()==-1):
-					break;
+					break
 		except KeyboardInterrupt:
 			if connection_exist:
 				connection.close()
@@ -75,4 +94,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	main()	
